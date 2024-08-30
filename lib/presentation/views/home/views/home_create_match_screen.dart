d79a14b00460a854a5a25be659094e7186a97119
Elmer Matthew Japara <elmer.japara@next-ti.co.id>
2024-08-30 15:55:48 +0700

    Add base GMaps (awaiting design innovation). Thanks for the free API KEY. (idiot devs are helpful)

--- lib/presentation/views/home/views/home_create_match_screen.dart
+++ lib/presentation/views/home/views/home_create_match_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:paddle_jakarta/presentation/common/ui_helpers.dart';
 import 'package:paddle_jakarta/presentation/views/home/viewmodels/home_viewmodel.dart';
 
 class HomeCreateMatchScreen extends StatelessWidget {
@@ -7,7 +9,27 @@ class HomeCreateMatchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.transparent,
+      child: ListView(
+        children: [
+          Text('Create a match', style: Theme.of(context).textTheme.titleLarge),
+          verticalSpaceLarge,
+          AspectRatio(
+            aspectRatio: 9 / 16,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: const GoogleMap(
+                initialCameraPosition: const CameraPosition(
+                  target: LatLng(-6.2088, 106.8456),
+                  zoom: 12.0,
+                ),
+              )
+            ),
+          )
+        ],
+      )
+    );
   }
 
 }
